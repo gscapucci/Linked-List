f719446419e9a59e85a56977e8b614b2164abc94
Guilherme Capucci <gscapucci@hotmail.com>
2021-05-10 20:10:06 -0300

    add operator "=" and method "get"

--- test/string.cpp
+++ test/string.cpp
@@ -1,11 +1,13 @@
 #include "../linkedlist.hpp"
 #include <string>
 #include <iostream>
-
+//
+//You need to overload operators to work with classes
+//
 class String{
-public:
+private:
     std::string str;
-
+public:
     String(std::string _str)
         :str(_str){}
 
@@ -24,8 +26,7 @@ public:
 
     bool operator==(const char* other)
     {
-        std::string _str(other);
-        if(this->str.compare(_str))
+        if(this->str.compare(other))
             return false;
         return true;
     }
@@ -37,6 +38,26 @@ public:
         return true;
     }
 
+    void operator=(String& other)
+    {
+        this->str = other.str;
+    }
+
+    void operator=(std::string& other)
+    {
+        this->str = other;
+    }
+
+    void operator=(const char* other)
+    {
+        this->str = other;
+    }
+
+
+    std::string get()
+    {
+        return this->str;
+    }
     friend std::ostream& operator<<(std::ostream& os, String self);
 };
 
